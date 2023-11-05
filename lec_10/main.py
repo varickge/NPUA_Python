12465940d82d78ed0fdca2650f8ccc8dac9fe1df
Varuzhan <varuzhangevorgian@gmail.com>
2023-11-05 14:40:57 +0400

    Added homework for lec_10.

---
+++ lec_10/main.py
@@ -0,0 +1,18 @@
+from vehicle import Vehicle
+
+class Car(Vehicle):
+    def __init__(self):
+        super().__init__()
+
+class Plane(Vehicle):
+    def __init__(self):
+        super().__init__()
+
+class Boat(Vehicle):
+    def __init__(self):
+        super().__init__()
+
+class RaceCar(Car):
+    def __init__(self):
+        super().__init__()
+
